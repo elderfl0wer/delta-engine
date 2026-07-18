41072c9b028327864abe9d59d8703b35088ff030
elderfl0wer <veckmanconstantiam@gmail.com>
2026-07-19 05:23:59 +0530

    added circle and point struct

--- include/delta/physics/rigidbody2d.h
+++ include/delta/physics/rigidbody2d.h
@@ -16,6 +16,17 @@ typedef struct {
 typedef enum {
     SHAPE_CIRCLE,
     SHAPE_BOX,
+    SHAPE_POINT,
 } SHAPE;
+
+typedef struct {
+    double radius;
+    vec2 center;
+} Circle;
+
+// Only position makes this useless ? Make all non-shaped bodies point objects be default ?
+typedef struct {
+    vec2 position;
+} Point;
 
 #endif /* RIGIDBODY2D_H */
