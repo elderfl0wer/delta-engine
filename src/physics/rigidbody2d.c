86dcba39a3920a32a92272420a01bb618425cf83
elderfl0wer <veckmanconstantiam@gmail.com>
2026-07-20 12:55:19 +0530

    fixed rbd_acculuate_gravity added accel to force

--- src/physics/rigidbody2d.c
+++ src/physics/rigidbody2d.c
@@ -47,7 +47,7 @@ void rb2d_apply_accel(RigidBody2D *rb, vec2 accel)
 
 void rb2d_accumulate_gravity(RigidBody2D *rb)
 {
-    rb->force.y += ACCEL_EARTH_GRAVITY;
+    rb->acceleration.y += ACCEL_EARTH_GRAVITY;
 }
 
 void rb2d_object_gravity(RigidBody2D *a, RigidBody2D *b)
