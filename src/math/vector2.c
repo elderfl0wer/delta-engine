a12a3547f110a2c5d2b645637471c8b61042a6f4
elderfl0wer <veckmanconstantiam@gmail.com>
2026-07-21 00:42:57 +0530

    removed seperate impl for vertical and horizontal angle

--- src/math/vector2.c
+++ src/math/vector2.c
@@ -71,18 +71,4 @@ double vec2_distance(vec2 *a, vec2 *b)
     return vec2_length(&ans);
 }
 
-double vec2_horizontal_angle(vec2 *v)
-{
-    vec2 horizontal = {1, 0};
-    double ans = acos(vec2_dot(v, &horizontal) / (vec2_length(v)*vec2_length(&horizontal)));
 
-    return ans;
-}
-
-double vec2_vertical_angle(vec2 *v)
-{
-    vec2 vertical = {0, 1};
-    double ans = acos(vec2_dot(v, &vertical) / (vec2_length(v)*vec2_length(&vertical)));
-
-    return ans;
-}
