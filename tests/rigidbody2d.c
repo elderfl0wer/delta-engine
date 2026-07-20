7799520b12bda2f4c01b0ff1235b73cb3801ce86
elderfl0wer <veckmanconstantiam@gmail.com>
2026-07-20 12:34:27 +0530

    simple test example to constantly update pos

--- tests/rigidbody2d.c
+++ tests/rigidbody2d.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "../include/delta/physics/rigidbody2d.h"
 #include "../include/delta/math/vector3.h"
@@ -6,7 +7,17 @@
 
 int main(void)
 {
-do {
-rb
-}while ()
+    RigidBody2D rb;
+    rb.position.x = 1;
+    rb.position.y = 2;
+
+    do {
+        vec2 gravity = {0.0f, -9.81f};
+
+        rb2d_apply_force(&rb, gravity);
+
+
+
+        rb2d_update_position(&rb, 0.1f);
+    } while (true);
 }
