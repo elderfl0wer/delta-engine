0eaea703c5953f6a2498105ac82ae994c27f8898
elderfl0wer <veckmanconstantiam@gmail.com>
2026-07-24 13:39:15 +0530

    added charge property to RigidBody2D and fundamental charge value to constants

--- include/delta/constants.h
+++ include/delta/constants.h
@@ -7,5 +7,6 @@
 // Physics
 #define FREE_FALL_ACCEL -9.80665f
 #define GRAVITATIONAL_CONSTANT (6.6743f*(pow(10, -11)))
+#define FUNDAMENTAL_CHARGE (1.602176634f*(pow(10, -19)))
 
 #endif /* CONSTANTS_H */
