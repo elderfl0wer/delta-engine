78dbd9e2b0d24be03ca23ddd79a7421c5af4f8e2
elderfl0wer <veckmanconstantiam@gmail.com>
2026-07-26 04:16:00 +0530

    coloumb constant & vacuum permittivity

--- include/delta/constants.h
+++ include/delta/constants.h
@@ -8,5 +8,7 @@
 #define FREE_FALL_ACCEL -9.80665f
 #define GRAVITATIONAL_CONSTANT (6.6743f*(pow(10, -11)))
 #define FUNDAMENTAL_CHARGE (1.602176634f*(pow(10, -19)))
+#define VACUUM_PERMITTIVITY (8.854187817f*(pow(10, -12)))
+#define COLOUMB_CONSTANT (8.99f*pow(10, 9))
 
 #endif /* CONSTANTS_H */
